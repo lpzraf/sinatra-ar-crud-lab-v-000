567d7b696328619b405bcaa4e2d8c9a37df97b3a
Rafael Lopez <lpz.raf@gmail.com>
2019-01-10 01:30:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
     post '/articles/:id/delete' do
      @article = Article.find_by_id(params[:id])
      @article.destroy
-    redirect :/articles
+    redirect :'/articles'
    end
   
 end
